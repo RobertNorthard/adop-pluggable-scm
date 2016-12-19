cba2172e5288456e007444ef1d0db83f9e8506e4
Northard, Robert A <robert.a.northard@accenture.com>
2016-12-19 18:26:11 +0000

    Update SCMProviderDataStore getAll method return type.

--- src/main/groovy/pluggable/scm/SCMProviderDataStore.groovy
+++ src/main/groovy/pluggable/scm/SCMProviderDataStore.groovy
@@ -3,13 +3,18 @@ package pluggable.scm;
 
 import java.util.Properties;
 
+/**
+*
+*
+* @author Robert Northard <robertnorthard@googlemail.com>
+*/
 public interface SCMProviderDataStore {
 
   /**
   * Return a string representation, in an array of all the available SCM providers.
   * @return a string representation, in an array of all the available SCM providers.
   */
-  public String[] getAll();
+  public List<String> getAll();
 
   /**
   * Return properties object for the specified SCM provider id.
