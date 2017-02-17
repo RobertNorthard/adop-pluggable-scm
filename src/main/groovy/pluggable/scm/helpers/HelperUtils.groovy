c5a4cb28a7035604e331620560f96b46c249dd52
Irmantas Marozas (Tony) <irmantas.t.marozas@accenture.com>
2017-02-17 14:24:58 +0000

    Remove credential manager into load cartridge.

--- src/main/groovy/pluggable/scm/helpers/HelperUtils.groovy
+++ src/main/groovy/pluggable/scm/helpers/HelperUtils.groovy
@@ -1,38 +1,11 @@
 package pluggable.scm.helpers;
-@GrabResolver(name='jenkins-ci.plugins', root='http://repo.jenkins-ci.org/releases/')
-@Grab(group='org.jenkins-ci.plugins', module='credentials', version='2.1.5')
-import com.cloudbees.plugins.credentials.*;
-import com.cloudbees.plugins.credentials.common.*;
+
 import java.util.Properties;
 
 /**
-* A helper class which contain helphull methods.
+* A helper class which contain helpful methods.
 */
 public class HelperUtils {
-
-  /**
-  * Get user name and password from credentialsID.
-  *
-  *  @param String credentials id.
-  *  @return String two values first is username, second password.
-  *  @throw IllegalArgumentException
-  *            If credentialId is null or empty.
-  */
-  public static String[] extractPasswordCredentials(String credentialId){
-        if(credentialId == null || credentialId.equals("")){
-            throw new IllegalArgumentException("Credential id not valid.");
-        }
-
-        def username_matcher = CredentialsMatchers.withId(credentialId);
-
-        def available_credentials = CredentialsProvider.lookupCredentials(
-                StandardUsernameCredentials.class
-        );
-
-        return [CredentialsMatchers.firstOrNull(available_credentials, username_matcher).username,
-                CredentialsMatchers.firstOrNull(available_credentials, username_matcher).password];
-  }
-
   /**
   * Get Properties from a file.
   *
